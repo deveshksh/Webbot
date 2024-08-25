3818c9ba6e7147031f065f27f4c5e1b7d09829f6
deveshksh <dkshandilya12345@gmail.com>
2024-08-25 17:56:50 +0530

    Initial commit

---
+++ src/components/Messages.tsx
@@ -0,0 +1,25 @@
+import { type Message as TMessage } from "ai/react";
+import { Message } from "./Message";
+
+interface MessagesProps {
+  messages: TMessage[];
+}
+
+export const Messages = ({ messages }: MessagesProps) => {
+  return (
+    <div className="flex max-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col overflow-y-auto">
+      {messages.length ? (
+        messages.map((message, i) => (
+          <Message key={i} content={message.content} isUserMessage={message.role === "user"} />
+        ))
+      ) : (
+        <div className="flex-1 flex flex-col items-center justify-center gap-2">
+          <img src="/assets/webbot_header.svg" alt="Webbot Logo" className="w-1000 h-2000" />
+          <h3 className="font-semibold text-xl text-white">Welcome to Webbot!</h3>
+          <p className="text-zinc-500 text-sm">You're all set!</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
